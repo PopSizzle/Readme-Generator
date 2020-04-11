1d84c44d2f04c46bf3c9599764cc87bbee786402
Sam Poppe <poppe.samuel@gmail.com>
2020-04-10 23:02:41 -0700

    combined into one file

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var inquirer = require("inquirer");
 var fs = require("fs");
 var axios = require("axios");
-var generateMarkdown = require("./Develop/utils/generateMarkdown.js");
 var avatarURL= "";
 var githubEmail= "";
 
@@ -79,5 +78,62 @@ function writeToFile(fileName, data) {
     console.log("Success");
     })
 }
+
+function generateMarkdown(data) {
+    dependencies = data.necessary.split(",");
+    let dependencyList= "";
+    acknowl = data.contributors.split(",");
+    let acknowlList= "";
+  
+    for(var i=0; i<dependencies.length; i++){
+      dependencyList += "\n  * " + dependencies[i];
+    }
+  
+    for(var i=0; i<acknowl.length; i++){
+      acknowlList += "\n  * " + acknowl[i];
+    }
+  
+    return `
+    # ${data.title}
+  
+    ${data.description}
+    
+    ## Table of Contents
+    
+    *Installation
+    *Usage
+    *License
+    *Contributors
+    *Tests
+    *Questions
+    *Contact Info
+    
+    ### Installation
+    
+    In order to use this Readme Generator you will need to install the following dependencies:
+    ${dependencyList}
+  
+    ### Usage
+    
+    ${data.usage}
+    
+    ## License
+    
+    ![MIT License](https://img.shields.io/badge/license-MIT-blue.svg)
+    
+    ## Acknowledgments
+    ${acknowlList}
+    
+    ## Tests
+  
+    ${data.tests}
+    
+    ## Questions
+    
+    * <img src="${avatarURL}" alt="avatar" style="border-radius: 16px" width="30">
+    * https://github.com/${data.username}
+    * ${githubEmail}
+  `;
+  }
 
 init();
--- readYou.md
+++ readYou.md
@@ -1,42 +1,43 @@
 
-  # p
-
-  p
+    # p
   
-  ## Table of Contents
-  
-  *Installation
-  *Usage
-  *License
-  *Contributors
-  *Tests
-  *Questions
-  *Contact Info
-  
-  ### Installation
-  
-  In order to use this Readme Generator you will need to install the following dependencies:
-  
-  * p
-
-  ### Usage
-  
-  p
-  
-  ## License
-  
-  ![MIT License](https://img.shields.io/badge/license-MIT-blue.svg)
-  
-  ## Acknowledgments
-  
+    p
+    
+    ## Table of Contents
+    
+    *Installation
+    *Usage
+    *License
+    *Contributors
+    *Tests
+    *Questions
+    *Contact Info
+    
+    ### Installation
+    
+    In order to use this Readme Generator you will need to install the following dependencies:
+    
   * p
   
-  ## Tests
-
-  p
+    ### Usage
+    
+    p
+    
+    ## License
+    
+    ![MIT License](https://img.shields.io/badge/license-MIT-blue.svg)
+    
+    ## Acknowledgments
+    
+  * p
+    
+    ## Tests
   
-  ## Questions
+    p
+    
+    ## Questions
+    
+    * <img src="https://avatars3.githubusercontent.com/u/60407759?v=4" alt="avatar" style="border-radius: 16px" width="30">
+    * https://github.com/PopSizzle
+    * null
   
-  * <img src="[object Object]" alt="avatar" style="border-radius: 16px" width="30">
-  * https://github.com/PopSizzle
-  * [object Object]
